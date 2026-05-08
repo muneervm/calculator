12a5ce414274184d5108f262b9bffc89a929f550
muneervm <vmmohammed88@gmail.com>
2026-05-08 10:37:11 +0530

    changes

---
+++ lib/Screens/splach_screen.dart
@@ -0,0 +1,61 @@
+import 'package:calculator/Common_Widgets/screen_resolution.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+
+    _opacityAnimation = Tween<double>(begin: 1.0, end: 0.2).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOutSine),
+    );
+
+    _controller.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 75, 165, 130),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Opacity(
+              opacity: _opacityAnimation.value,
+              child: SizedBox(
+                width: SizeConfig.blockHeight * 15,
+                height: SizeConfig.blockHeight * 15,
+                child: Image.asset('assets/icon.png', fit: BoxFit.contain),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
